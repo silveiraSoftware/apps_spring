b4e88b6c17d1623769b843fd17367c12964cbbcd
adalbertosn1982 <adalbertosn1982@gmail.com>
2020-05-21 23:49:36 -0300

    atualizando controler principal do projeto

--- exemplo1apprest/src/main/java/br/com/adalberto/prjava/exemplo1apprest/controllers/InicalController.java
+++ exemplo1apprest/src/main/java/br/com/adalberto/prjava/exemplo1apprest/controllers/InicalController.java
@@ -1,6 +1,8 @@
 package br.com.adalberto.prjava.exemplo1apprest.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,10 @@ public class InicalController {
 	@RequestMapping("/hello")
 	public String hello() {
 		return "hello";
+	}
+	
+	@GetMapping("/hello1")
+	public String hello1(@RequestParam(value = "name", defaultValue = "World") String name) {
+	return String.format("Hello %s!", name);
 	}
 }
